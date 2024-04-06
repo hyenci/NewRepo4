8692688ea2e6b09adef54e23c638896b785810cc
hyenci21 <159926659+hyenci21@users.noreply.github.com>
2024-04-06 10:55:41 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp23/Clases/Carro.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23.Clases
+{
+    public class Carro
+    {
+
+
+
+        public string Marca { get; }
+        public int Modelo { get; }
+        public string Color { get; set; }
+
+        public string owner { get; set; }
+
+        private int Encendido = 0;
+        private int velocidad_actual = 0;
+        private int MAXVELOCIDAD { get; set; }
+
+
+
+
+        public Carro(string m, int model, string col)
+        {
+
+            Marca = m;
+
+
+            Modelo = model;
+            Color = col;
+
+        }
+
+
+
+        public int Acelerar()
+        {
+
+            velocidad_actual += 10;
+            return velocidad_actual;
+
+
+            if (Encendido == 0)
+            {
+
+              
+                return 0;
+
+
+            }
+            else
+            {
+
+                velocidad_actual += 10;
+                return velocidad_actual;
+
+
+            }
+
+         
+        }
+
+
+    }
+}
+
+
+
+   
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp23.Clases;
+
+Carro carrito = new Carro("toyota",2024, "blanco");
+Carro carro2 = new Carro("Nissan", 2020, "Azul");
+
+carrito.Color = "Rojo";
+carrito.owner = "Juan";
+
+
+
+
+
+
+//Console.WriteLine($"El carro es un {carrito.Marca}");
+//Console.WriteLine($"El carro es un {carro2.Marca}");
+
+
+
+Console.WriteLine(carrito.Acelerar());
+Console.WriteLine(carrito.Acelerar());//ES ACELERAR 3 VECES
+Console.WriteLine(carrito.Acelerar());
+
+if (carrito.Acelerar() == 0)
+{
+
+    Console.WriteLine("El carro esta apagado");
+
+}
+else
+{
+    Console.WriteLine(carrito.Acelerar()+"/KPH");
+}
